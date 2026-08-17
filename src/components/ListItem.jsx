faf32d433c65612fd947652592cc9d1175fe998d
mobinaj94 <mobinajafari94@yahoo.com>
2026-08-17 16:54:30 +0200

    the Are you sure part for the delete function, added

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function ListItem({
@@ -10,11 +10,15 @@ function ListItem({
   foodList,
   setFoodList,
 }) {
-  function handleDelete(id) {
+  // For "Are you sure functionality" a new state is defined
+  const [showModal, setShowModal] = useState(false);
+
+  function handleDelete() {
     const newFoodList = foodList.filter((food) => {
       return food.id !== id;
     });
     setFoodList(newFoodList);
+    setShowModal(false);
   }
 
   return (
@@ -27,14 +31,38 @@ function ListItem({
         <h3>{name}</h3>
         <p>calories: {calories}</p>
         <p>servings: {servings}</p>
-        {calories > 400 ? <p> High in calories</p> : null}
+        {calories > 400 ? <p>High in calories</p> : <p>Healthy</p>}
+        {/*delete*/}
+        <button onClick={() => setShowModal(true)}>Delete</button>
 
-        <button onClick={() => handleDelete(id)}>Delete</button>
+        {/*details*/}
         <Link to={`/${id}`}>Link</Link>
+
+        {/*edit*/}
         <button>
           <Link to={`/edit/${id}`}>Edit</Link>
         </button>
       </div>
+
+        {/*Are you sure part*/}
+      {showModal && (
+        <div id="modal">
+          <div
+            id="modalBox"
+            style={{
+              backgroundColor: "gold",
+              padding: "20px",
+              borderRadius: "8px",
+              width: "300px",
+              textAlign: "center",
+            }}
+          >
+            <h3>Are You Sure?</h3>
+            <button id="yes-btn" onClick={handleDelete}>Yes,Delete</button>
+            <button id="cancel-btn" onClick={() => setShowModal(false)}>Cancel</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
